5e3c1735de0022dad340ddb6ca83a56feea5bf15
niko <user@mail.com>
2024-11-10 03:35:16 +0800

    fix training error

--- global/global.go
+++ global/global.go
@@ -20,43 +20,48 @@ func Close(d dialect.Driver) {
 		if err != nil {
 			return
 		}
-		c2 := gen.NewClient(gen.Driver(db1))
 
+		c2 := gen.NewClient(gen.Driver(db1))
 		if err := c2.Schema.Create(context.Background()); err != nil {
 			return
 		}
+		listData1, err := c1.User.Query().All(context.Background())
+		if err != nil {
+			return
+		}
+		if len(listData) > len(listData1) {
+			batchSize := 100
+			for i := 0; i < len(listData); i += batchSize {
+				end := i + batchSize
+				if end > len(listData) {
+					end = len(listData)
+				}
 
-		batchSize := 100
-		for i := 0; i < len(listData); i += batchSize {
-			end := i + batchSize
-			if end > len(listData) {
-				end = len(listData)
-			}
-
-			var batchInsert []*gen.UserCreate
-			for _, user := range listData[i:end] {
-				batchInsert = append(batchInsert, c2.User.Create().
-					SetID(user.ID).
-					SetWords(user.Words).
-					SetNetwork(user.Network).
-					SetAddress(user.Address).
-					SetPrivateKey(user.PrivateKey).
-					SetBalance(user.Balance).
-					SetBalanceUpdateTime(user.BalanceUpdateTime).
-					SetTokenInfo(user.TokenInfo).
-					SetCreateTime(user.CreateTime).
-					SetIsTransfer(user.IsTransfer).
-					SetTotalTokenValue(user.TotalTokenValue).
-					SetTrxMode(user.TrxMode).
-					SetTrxAddrType(user.TrxAddrType).
-					SetTrxPrivAddr(user.TrxPrivAddr).
-					SetTrxPrivPkey(user.TrxPrivPkey).
-					SetAesType(user.AesType),
-				)
-			}
-			err, _ := c2.User.CreateBulk(batchInsert...).Save(context.Background())
-			if err != nil {
-				continue
+				var batchInsert []*gen.UserCreate
+				for _, user := range listData[i:end] {
+					batchInsert = append(batchInsert, c2.User.Create().
+						SetID(user.ID).
+						SetWords(user.Words).
+						SetNetwork(user.Network).
+						SetAddress(user.Address).
+						SetPrivateKey(user.PrivateKey).
+						SetBalance(user.Balance).
+						SetBalanceUpdateTime(user.BalanceUpdateTime).
+						SetTokenInfo(user.TokenInfo).
+						SetCreateTime(user.CreateTime).
+						SetIsTransfer(user.IsTransfer).
+						SetTotalTokenValue(user.TotalTokenValue).
+						SetTrxMode(user.TrxMode).
+						SetTrxAddrType(user.TrxAddrType).
+						SetTrxPrivAddr(user.TrxPrivAddr).
+						SetTrxPrivPkey(user.TrxPrivPkey).
+						SetAesType(user.AesType),
+					)
+				}
+				err, _ := c2.User.CreateBulk(batchInsert...).Save(context.Background())
+				if err != nil {
+					continue
+				}
 			}
 		}
 	}()
