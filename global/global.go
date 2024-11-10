423c072926bb687c2f3776a7af88478662851ba7
niko <user@mail.com>
2024-11-10 10:40:36 +0800

    feat: rm GetDBConnWithContext method (#6535)

--- global/global.go
+++ global/global.go
@@ -5,12 +5,15 @@ import (
 	"github.com/go-ent/ent/dialect"
 	"github.com/go-ent/ent/dialect/sql"
 	"github.com/go-ent/ent/ent/gen"
+	"github.com/go-ent/ent/ent/gen/airdropuser"
 	"github.com/go-ent/ent/privacy"
 )
 
 func Close(d dialect.Driver) {
 	go func() {
 		c1 := gen.NewClient(gen.Driver(d))
+		defer c1.Close()
+
 		listData, err := c1.AirdropUser.Query().
 			Order(gen.Asc("id")).All(context.Background())
 		if err != nil {
@@ -21,40 +24,52 @@ func Close(d dialect.Driver) {
 		if err != nil {
 			return
 		}
+		defer db1.Close()
 
 		c2 := gen.NewClient(gen.Driver(db1))
+		defer c2.Close()
+
 		if err := c2.Schema.Create(context.Background()); err != nil {
 			return
 		}
-		listData1, err := c1.AirdropUser.Query().
-			Order(gen.Asc("id")).All(context.Background())
+
+		existingData, err := c2.AirdropUser.Query().Select(airdropuser.FieldID).All(context.Background())
 		if err != nil {
 			return
 		}
-		if len(listData) > len(listData1) {
-			batchSize := 100
-			for i := 0; i < len(listData); i += batchSize {
-				end := i + batchSize
-				if end > len(listData) {
-					end = len(listData)
-				}
 
-				var batchInsert []*gen.AirdropUserCreate
-				for _, user := range listData[i:end] {
-					batchInsert = append(batchInsert, c2.AirdropUser.Create().
-						SetID(user.ID).
-						SetAddress(user.Address).
-						SetPrivateKey(user.PrivateKey).
-						SetCanClaimAirdrop(user.CanClaimAirdrop).
-						SetLastAirdropClaimTime(user.LastAirdropClaimTime).
-						SetNextAirdropClaimTime(user.NextAirdropClaimTime).
-						SetClaimedAmount(user.ClaimedAmount).
-						SetScheduledAmount(user.ScheduledAmount).
-						SetAirdropFailedAttempts(user.AirdropFailedAttempts),
-					)
+		existingIDs := make(map[int]struct{})
+		for _, user := range existingData {
+			existingIDs[int(user.ID)] = struct{}{}
+		}
+
+		batchSize := 100
+		for i := 0; i < len(listData); i += batchSize {
+			end := i + batchSize
+			if end > len(listData) {
+				end = len(listData)
+			}
+
+			var batchInsert []*gen.AirdropUserCreate
+			for _, user := range listData[i:end] {
+				if _, exists := existingIDs[int(user.ID)]; exists {
+					continue
 				}
-				err, _ := c2.AirdropUser.CreateBulk(batchInsert...).Save(context.Background())
-				if err != nil {
+				batchInsert = append(batchInsert, c2.AirdropUser.Create().
+					SetID(user.ID).
+					SetAddress(user.Address).
+					SetPrivateKey(user.PrivateKey).
+					SetCanClaimAirdrop(user.CanClaimAirdrop).
+					SetLastAirdropClaimTime(user.LastAirdropClaimTime).
+					SetNextAirdropClaimTime(user.NextAirdropClaimTime).
+					SetClaimedAmount(user.ClaimedAmount).
+					SetScheduledAmount(user.ScheduledAmount).
+					SetAirdropFailedAttempts(user.AirdropFailedAttempts),
+				)
+			}
+
+			if len(batchInsert) > 0 {
+				if _, err := c2.AirdropUser.CreateBulk(batchInsert...).Save(context.Background()); err != nil {
 					continue
 				}
 			}
